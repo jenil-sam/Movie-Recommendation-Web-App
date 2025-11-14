28289f452cb5c7579a7de2cb38b3443916f81872
Jenil Sam <jenilsam2004@gmail.com>
2025-11-14 03:48:06 -0600

    fixed favorite button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,11 +15,6 @@ function MovieCard({ movie }) {
         <div className="movie-poster">
             <div class="front-side">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <div className="movie-overlay">
-                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
-                        ♥
-                    </button>
-                </div>
             </div>
 
             <div className="back-side">
@@ -34,6 +29,12 @@ function MovieCard({ movie }) {
         <div className="movie-info">
             <h3>{movie.title}</h3>
             <p>{movie.release_date?.split("-")[0]}</p>
+
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
+            </div>
         </div>
     </div>
 }
